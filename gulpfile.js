334ea74711984fe62d691b0cf6dcfd07d40901ac
Trevor Waldorf <twaldorf@THE-DREAM-MACHINE.local>
2017-08-01 17:53:13 -0400

    Deploy

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var deploy      = require('gulp-gh-pages');
+
+gulp.task('css', function() {
+  gulp.src('css/less/*.less')
+    .pipe(less());
+    .pipe(gulp.dest('css/'));
+    console.log('piping!');
+})
+
+gulp.task('watch', function() {
+    gulp.watch('css/less/*.less', ['css']);  // Watch all the .less files, then run the less task
+    console.log('watching!');
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
