07979e1140577e5594dd1b08a230efba1dbb6051
Trevor Waldorf <trevor.automated@gmail.com>
2017-08-08 17:35:00 -0400

    add Honeycomb animation core

--- js/honeycomb.js
+++ js/honeycomb.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  $('#comb-1').click(function() {
-    $('#comb-1-container').slideToggle(180);
-    console.log('toggle');
-    $('#hexagon-1').toggleClass('.far');
-    // $('#line').
+  $('.comb').next().hide();
+  $('.comb').click(function() {
+    // $(this).toggleClass('rear');
+    $(this).next().slideToggle(300, 'swing');
+    $(this).toggleClass('active');
+    $(this).parent().toggleClass('active');
+    console.log($(this).next($('div.comb-container')));
+    console.log($(this));
+    // $('#comb-1').toggleClass('.far');
   })
 })
