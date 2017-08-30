f36ef00b842e2df5fad550a0604b1b55c4b4aa42
Trevor Waldorf <trevor.automated@gmail.com>
2017-08-30 16:52:42 -0400

    style layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 var deploy      = require('gulp-gh-pages');
 
 gulp.task('css', function() {
-  gulp.src('css/less/*.less')
+  gulp.src('css/less/[^_]*.less')
     .pipe(less())
     .pipe(gulp.dest('css/'))
-    console.log('piping!');
+    console.log('piping!')
 })
 
 gulp.task('watch', function() {
@@ -14,9 +15,9 @@ gulp.task('watch', function() {
     console.log('watching!');
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'])
 
 gulp.task('deploy', function () {
-  return gulp.src("./**/*")
+  return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
